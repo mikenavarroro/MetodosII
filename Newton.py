8407bdceea47b8f395201df09f3d04d8006b389c
mikenavarroro <mike.navarroro@gmail.com>
2020-03-10 23:57:34 -0600

    Correción del punto

--- Newton.py
+++ Newton.py
@@ -1,6 +1,7 @@
 import numpy as np
 import sympy as sp
 
+funciones = [] #Guardar las funciones
 variables = [] #Guardar las variables de las funciones
 
 #Hacer símbolos el abecedario para poder derivar
@@ -13,16 +14,15 @@ abecedario = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, 
 #Pedir el número de variables del sistema
 n_var = int(input("Cuantas variables tiene tu sistema: "))
 
-funciones = np.array((n_var, 1)) #Guardar las funciones
-punto = np.array((n_var, 1)) #Guardar el punto inicial y posteriormente la solución
+punto = [] #Guardar el punto inicial y posteriormente la solución
 
 for i_var in range(n_var):
-    funciones[i_var, 0] = input("función {}: ".format(i_var+1)) #Agregar n funciones a la lista
+    funciones.append(input("función {}: ".format(i_var+1)) #Agregar n funciones a la lista)
 
-for i_var in range(n_var):                                           #
-    for j_var in funciones[i_var, 0]:                                #
+for i_var in range(n_var):
+    for j_var in funciones[i_var]:                                   #
         if j_var >= 'A' and j_var <= 'Z':                            #Convertir las variables
-            j_var = j_var.lower()                                    #en minusculas
+            j_var = j_var.lower()                                    #en minúsculas
         if j_var >= 'a' and j_var <= 'z':                            #Agregar las variables a la lista
             variables.append(j_var)                                  #y si alguna variable ya está, esta
             if j_var in variables and len(variables) > n_var:        #se elimina
@@ -31,7 +31,7 @@ variables.sort() #Ordenar las variables en orden alfabético
 
 #Ingresar el punto inicial
 for i_var in range(n_var):
-    punto[i_var, 0] = float(input("Ingresa el valor de {} en x0: ".format(variables[i_var])))
+    punto[i_var] = float(input("Ingresa el valor de {} en x0: ".format(variables[i_var])))
 
 #Pedir la tolerancia
 tol = float(input("Tolerancia: "))
@@ -41,13 +41,13 @@ iter = int(input("Numero máximo de iteraciones: "))
 Jacobi = np.empty((n_var, n_var)) #Matriz jacobina vacía
 func = [] #Lista para poder derivar
 der = [] #Lista que guarda las derivadas
-o_var = 1 #Número de iteraciones hechas
+no_iteraciones = 1 #Número de iteraciones hechas
 error = 1 #Error
 puntos_anteriores = [] #Cálculo del error
 
 #Hacer símbolos las funciones
 for i_var in range(n_var):
-    func.append(sp.sympify(funciones[i_var, 0]))
+    func.append(sp.sympify(funciones[i_var]))
 
 #Derivar las funciones con respecto a las variables
 for i_var in range(n_var):
@@ -65,24 +65,27 @@ for i_var in range(n_var):
 var_abc.sort() #Ordenar alfabéticamente las variables que se van a usar
 
 #Inicio del método de Newton
-while o_var < iter or error > tol:
+while no_iteraciones < iter or error > tol:
     #Evaluar la matriz jacobiana en el punto
     m_var = 0
     for i_var in range(n_var):
         for j_var in range(n_var):
-            Jacobi[i_var, j_var] = sp.sympify(der[m_var]).subs(var_abc[j_var], punto[j_var, 0])
+            Jacobi[i_var, j_var] = sp.sympify(der[m_var]).subs(var_abc[j_var], punto[j_var])
             m_var += 1
 
     JacobiInv = np.linalg.inv(Jacobi) #Inversa de la matriz
 
-    F = np.empty((n_var, 1)) #Crear la matriz de F(Xk)
+    F = [] #Crear la matriz de F(Xk)
 
     #Evaluar las funciones en el punto
     l_var = 0
     for i_var in range(n_var):
         for j_var in range(n_var):
-            F[i_var, 1] = sp.sympify(funciones[l_var, 0]).subs(var_abc[j_var], punto[j_var, 0])
+            F.append(sp.sympify(funciones[l_var]).subs(var_abc[j_var], punto[j_var]))
             l_var += 1
+    
+    F = np.asarray(F)
+    F = np.linalg.transpose(F)
 
     Y = JacobiInv @ F #Multiplicación de la inversa del jacobiano con el vector F(Xk)
 
@@ -93,12 +96,16 @@ while o_var < iter or error > tol:
     #Calcular el error
     p_var = 0
     for i_var in puntos_anteriores:
-        error = max(abs(punto[p_var, 0] - i_var))
+        error = max(abs(punto[p_var] - i_var))
 
-    o_var += 1
+    no_iteraciones += 1
+
+
+punto = np.asarray(punto)
+punto = np.linalg.transpose(punto)
 
 #Imprimir el resultado
-if o_var > iter:
+if no_iteraciones > iter:
     print("El número de iteraciones fue excedido.")
     print("La solución es ", punto)
 elif error < tol:
